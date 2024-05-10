1a51d26fb5c4ce095eac0a30b2755ce4393186f6
Md. Shakil Rana <159621858+MDshakilr@users.noreply.github.com>
2024-05-10 12:41:02 +0600

    Update Factorial.cpp

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -1,3 +1,30 @@
+/*
+Basic code ::
+#include <iostream>
+using namespace std;
+int fun(int n)
+{ 
+    if(n==0)
+    {
+        return 1;
+    }
+    
+    else
+    {
+        return 7+fun(n-2);
+    }
+}
+
+int main()
+{
+    cout<<fun(4);
+}
+
+
+*/
+
+
+
 #include <iostream>
 using namespace std;
 
